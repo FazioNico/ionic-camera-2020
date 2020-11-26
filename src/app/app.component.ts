78d1ef8e89656f7489af42ad82adbb712c6cb6bd
FazioNico <contact@nicolasfazio.ch>
2020-11-26 11:58:46 +0100

    upd: corrigé caméra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IonButton } from '@ionic/angular';
+import { CameraService } from './services/camera/camera.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'IonicCamera2020';
+  imgUrl: string = null;
+
+  constructor(
+    private _camera: CameraService
+  ) {}
+
+  async takePicture(btn: IonButton) {
+    btn.disabled = true;
+    this.imgUrl = await this._camera.takePicture();
+    btn.disabled = false;
+  }
 }
